305da26841eeb3c1c1600e7e1706667df97ebb17
Sebastian Spicker <sebastian.spicker@googlemail.com>
2023-10-01 09:23:00 +0200

    test(review): add a basic smoke pass for review

---
+++ src/review-followup.ts
@@ -0,0 +1,8 @@
+export function createReviewSummary() {
+  return { scope: "review", status: "ready" };
+}
+
+// current lane: review
+export function reviewTask() {
+  return { scope: "review", status: "ready" };
+}
